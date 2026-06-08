7c736bef72d5bea68def5cedf6227671d5ca9c5f
Namit Gawade <namitgawade5388@gmail.com>
2026-06-08 11:31:33 +0530

    docs(nyc-taxi): add architectural documentation and inline comments  - Added top-level block comment detailing the four optimization strategies - Added inline explanations for partitioning and clustering choices -

--- nyc_taxi_optimized.sql
+++ nyc_taxi_optimized.sql
@@ -1,14 +1,25 @@
+/* Transforms raw, expensive NYC Taxi data into a clean table 
+Architecture Applied:
+         - Columnar Pruning: Selects only required fields.
+         - Denormalization: Combine columns into a STRUCT.
+         - Partitioning: Partitioned by date to reduce data scan costs.
+         - Clustering: Clustered by vendor_id for fast filtering.
+*/
+
 CREATE OR REPLACE TABLE `gcp-learning-lab-498505.intern_sandbox.optimized_nyc_taxi_trips`
+-- Partitioning by pickup_datetime so that we can query specific days without scanning the whole year
 PARTITION BY DATE(pickup_datetime)
+-- Clustering by vendor_id for fast filtering between different taxi companies
 CLUSTER BY vendor_id
 AS
-SELECT 
-    vendor_id,
+SELECT     
+    vendor_id, -- Selecting only those columns which are required instead of selecting the full table
     pickup_datetime,
     dropoff_datetime,
     passenger_count,
     trip_distance,
-    -- Grouping the messy financials into a clean STRUCT
+    
+    -- Denormalizes unorganized data into a single, organized JSON-like format
     STRUCT(
         fare_amount, 
         tip_amount, 
@@ -18,4 +29,5 @@ SELECT
 FROM 
     `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022`
 WHERE 
+    -- Ignoring the anomalies in the table
     fare_amount > 0;
